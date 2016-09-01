39753232c019703718c56bb7c9be7b0ce952a9b0
Shemalatha <hemasubbu96@gmail.com>
2016-09-01 11:28:10 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if(a%2=0)
+{
+printf("even" +even);
+}
+else
+{
+printf("odd" +odd);
+}
+}
