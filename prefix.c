e3074994b4ca180836d7faa4d146a33dc4542924
Shemalatha <hemasubbu96@gmail.com>
2016-09-02 11:41:41 +0530

    Create prefix.c

---
+++ prefix.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void description(void)
+void prefix(char x1[],char x2[]);
+int main()
+{
+char x1[MAX];
+char x2[MAX];
+description();
+for(int i=0;i<5;i++)
+{
+printf("\n\n enter the first word");
+fgets(x1,MAX,stdin);
+x2[strlen(x2)-1]='\0';
+printf(%s,%s",x1,x2);
+prefix(x1,x2);
+}
+return(0);
+void description(void)
+{
+puts("iongest common prefix");
+}
+void prefix(char x1[],char x2[])
+{
+char x3[MAX];
+for(int i=0;i<MAX;++i)
+{
+while(x1[i]==x2[i])
+x3[i]=x1;
+}
+printf("\"%s.\"",x3);
+}
+getch();
+}
+
